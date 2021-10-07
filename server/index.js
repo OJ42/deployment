bc37b7ac28e997b29138a1cbd9155e33783beef3
OJ42 <88906115+OJ42@users.noreply.github.com>
2021-10-07 13:55:48 -0600

    style css

--- server/index.js
+++ server/index.js
@@ -7,16 +7,13 @@ app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
+app.get('/styles.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../style.css'))
+})
+
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
 
-app.get('/js', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.js'))
-})
-
-app.get('/styles.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '../style.css'))
-})
